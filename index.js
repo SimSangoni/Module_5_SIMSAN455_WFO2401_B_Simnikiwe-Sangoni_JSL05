d0dd4a3bc3b3d3137a5ca1ea4ec73aceb2226368
Simnikiwe Sangoni <simnikiwesangoni@gmail.com>
2024-03-27 16:57:42 +0200

    Additions of each guardians preferred genre

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ const songs = [
 const guardians = {
     "Star-Lord": "Rock",
     "Gamora": "Pop",
+    "Drax": "R&B",
+    "Rocket": "Dance",
+    "Groot": "Hip-Hop/Rap",
     // Add preferences for Drax, Rocket, and Groot
 };
 
